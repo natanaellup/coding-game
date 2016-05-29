b7a670c7de7d3aeaefa00995dd91d4eae5b26569
naty.claudiu <naty.claudiu@gmail.com>
2016-05-29 17:06:18 +0300

    make changes user permission in language and lesson page.

--- src/LessonBundle/Controller/LessonController.php
+++ src/LessonBundle/Controller/LessonController.php
@@ -15,9 +15,11 @@ use Doctrine\Common\Proxy\Exception\InvalidArgumentException;
 use LessonBundle\Entity\Lesson;
 use LessonBundle\Services\QuestionService;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class LessonController extends Controller
 {
@@ -27,12 +29,18 @@ class LessonController extends Controller
     {
         $service = $this->get('lesson_bundle.lesson_service');
         $doctrine = $this->getDoctrine();
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+
+        if(is_null($user) || $user=='anon.'){
+            $this->redirectToRoute('fos_user_security_login');
+        }
+
         /**
          * @var $lesson Lesson
          */
         $lesson = $service->getLessonById($id);
         $activityService = new ActivityTracking($doctrine, $this->get('security.token_storage'));
-        if($activityService->getLanguageTotalScore($lesson->getLanguage()) < $lesson->getScoreToUnlock()){
+        if($activityService->getLanguageTotalScore($lesson->getLanguage(), $user) < $lesson->getScoreToUnlock()){
             return new Response('You are not allowed to view this lesson. Please collect more than ' . $lesson->getScoreToUnlock() . ' experience points (xp) to access this lesson.');
         }
         return $this->render('LessonBundle:Lesson:show.html.twig', array('lesson' => $lesson, 'questionService' => new QuestionService($doctrine)));
